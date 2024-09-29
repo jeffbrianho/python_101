467029ab828e11635db41f55008c3ed2081eceda
Jeffrey Ho <jeffbrianho@gmail.com>
2024-09-29 08:43:53 -0500

    updated plyinth, refactor calulator, checked hello, started pseudocode

---
+++ lesson_2/calculator.py
@@ -0,0 +1,51 @@
+# Ask for a first number
+# Ask for a second number
+# Ask for the type of operation
+# Print out the value of the operation from number 1 and 2
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+    
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt('What is the first number?')
+number1 = (input())
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt('What is the second number?')
+number2 = (input())
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt('What is the operation you would like to perform?'
+      '\n 1) Add 2) Subtract 3) Multiply 4) Divide')
+operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
+
+match operation:
+
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)   
+    case '4':
+        output = int(number1) / int(number2)         
+
+prompt(f'The result is: {output}')
---
+++ lesson_2/pseudocode.py
@@ -0,0 +1,60 @@
+# 1) a function that returns the sum of two numbers
+
+# 2) a function that takes a list of strings, and returns a 
+#    string that is all those strings concatenated together
+
+# 3) a function that takes a list of integers, and returns
+#    a new list with every other element from the original list, 
+#    starting with the first element. For instance:
+
+# Keyword	Meaning
+# START	start of the program
+# SET	set a variable that we can use for later
+# GET	retrieve input from user
+# PRINT	display output to user
+# READ	retrieve a value from a variable
+# IF/ELSE IF/ELSE	show conditional branches in logic
+# WHILE	show looping logic
+# END	end of the program
+
+# 1) START 
+# make a function that takes 2 arguements and wil summate and print 
+# get 2 variable numbers calling the function 
+# print out the sum
+
+# def summate(x, y):
+#     print(int(x) + int(y))
+
+# summate(8, 4)
+
+# 2) START
+# create a function that will take a list of string and return a string cacatenation
+# SET a list of strings
+# SET a function to take a list and will concatenate the list
+# iterate throughout each element with for string in list
+# create a super string to add to each other
+
+# string_list = ['abd', 'def', 'ghi']
+
+# for index in string_list:
+#     print(string)
+    
+
+# 3) START 
+# SET a list of integers
+# Create a a function that iterates through a list
+# use a range for the index to iterate through every other number and pring
+# PRINT the integer
+
+# every_other = ([1, 4, 7, 2, 5])
+# index = 0
+# while index < len(every_other):
+#     print(every_other[index])
+#     index += 2
+    
+START
+SET a function that will take a String 
+use a find method or iterate through string to see if it occurs 3x
+index the location of the third time 
+print the location 
+
