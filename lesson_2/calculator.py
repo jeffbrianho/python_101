f2f7baec6867cea0848124f395ff85f7636646e4
Jeffrey Ho <jeffbrianho@gmail.com>
2024-10-02 08:01:28 -0500

    added continuationof calculator

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -12,38 +12,44 @@ def invalid_number(number_str):
         return True
     return False
 
-prompt('Welcome to Calculator!')
+while True:
+    prompt('Welcome to Calculator!')
 
-prompt('What is the first number?')
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt('What is the first number?')
     number1 = input()
 
-prompt('What is the second number?')
-number2 = input()
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt('What is the second number?')
     number2 = input()
 
-prompt('What is the operation you would like to perform?'
-      '\n 1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
+    prompt('What is the operation you would like to perform?'
+        '\n 1) Add 2) Subtract 3) Multiply 4) Divide')
     operation = input()
 
-match operation:
+    while operation not in ["1", "2", "3", "4"]:
+        prompt('You must choose 1, 2, 3, or 4')
+        operation = input()
 
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) * int(number2)
-    case '4':
-        output = int(number1) / int(number2)
-prompt(f'The result is: {output}')
+    match operation:
+
+        case '1':
+            output = int(number1) + int(number2)
+        case '2':
+            output = int(number1) - int(number2)
+        case '3':
+            output = int(number1) * int(number2)
+        case '4':
+            output = int(number1) / int(number2)
+    prompt(f'The result is: {output}')
+
+    prompt(f'Would you like to perform another calculation?(Y/N)')
+    response = input()
+    if response and response.upper() != 'Y':
+        break
