2f6dd9cc19b26c5a45485aa323b6d3dc25283ecb
Jeffrey Ho <jeffbrianho@gmail.com>
2024-10-13 12:10:48 -0500

    updated RPS

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -2,36 +2,64 @@
 
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 def prompt(message):
     print(f'===> {message}')
 
-def display_winner(player, computer):
-    if ((player == 'rock' and computer == 'scissors') or
-        (player == 'paper' and computer == 'rock') or
-        (player == 'scissors' and computer == 'paper')):
-        prompt('You Win')
-    elif ((player == 'rock' and computer == 'paper') or
-        (player == 'paper' and computer == 'scissors') or
-        (player == 'scissors' and computer == 'rock')):
-        prompt('Computer Wins!')
+def player_wins(players_choice, computers_choice):
+    if ((players_choice == 'rock' and computers_choice == 'lizard') or
+        (players_choice == 'rock' and computers_choice == 'scissors') or
+        (players_choice == 'paper' and computers_choice == 'spock') or
+        (players_choice== 'paper' and computers_choice == 'rock') or
+        (players_choice == 'scissors' and computers_choice == 'lizard') or
+        (players_choice == 'scissors' and computers_choice == 'paper') or
+        (players_choice == 'lizard' and computers_choice == 'paper') or
+        (players_choice == 'lizard' and computers_choice == 'spock') or
+        (players_choice == 'spock' and computers_choice== 'rock' ) or
+        (players_choice== 'spock' and computers_choice == 'scissors')
+        ):
+        return True
     else:
-        prompt("It's a Tie!")
+        return False
+
+def display_winner(player, computer):
+    if player_wins(player, computer):
+        prompt('You Win!')
+    elif player == computer:
+        prompt('It is a Tie!')
+    else:
+        prompt("Computer Wins!")
 
 while True:
-    prompt(f"Choose one: {', '.join(VALID_CHOICES)}")
-    choice = input()
 
-    while choice not in VALID_CHOICES:
-        prompt('That is not a valid choice')
+    PLAYER_WIN_COUNTER = 0
+    COMPUTER_WIN_COUNTER = 0
+
+    prompt(f"Welcome to {', '.join(VALID_CHOICES)}! Best of 5 Wins!")
+
+    while (PLAYER_WIN_COUNTER < 3) and (COMPUTER_WIN_COUNTER < 3):
+        prompt(f"Choose one: {', '.join(VALID_CHOICES)}")
         choice = input()
 
-    computer_choice = random.choice(VALID_CHOICES)
+        while choice not in VALID_CHOICES:
+            prompt('That is not a valid choice')
+            choice = input()
 
-    prompt(f'You chose {choice}, computer chose {computer_choice}')
+        computer_choice = random.choice(VALID_CHOICES)
 
-    display_winner(choice, computer_choice)
+        prompt(f'You chose {choice}, computer chose {computer_choice}')
+        display_winner(choice, computer_choice)
+
+        if choice == computer_choice:
+            continue
+        if player_wins(choice, computer_choice):
+            PLAYER_WIN_COUNTER += 1
+        else:
+            COMPUTER_WIN_COUNTER += 1
+
+        prompt(f'Player wins = {PLAYER_WIN_COUNTER},'
+        f'Computer wins = {COMPUTER_WIN_COUNTER}')
 
     prompt('Do you want to play again? (y/n)?')
     answer = input().lower()
