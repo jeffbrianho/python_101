072adcf161a6b23cfea5ba4aa1b213b6c513e1a6
Jeffrey Ho <jeffbrianho@gmail.com>
2024-10-12 19:22:55 -0500

    initial rock paper scissors

--- lesson_2/rps.py
+++ lesson_2/rps.py
@@ -8,11 +8,11 @@ def prompt(welcome):
 def choice(number):
     if int(number) == 1:
         return "Rock"
-    elif int(number) == 2:
+    if int(number) == 2:
         return "Paper"
-    elif int(number) == 3:
+    if int(number) == 3:
         return "Scissor"
-    
+
 while True:
     prompt('Welcome to Rock, Paper, Scissors!')
 
@@ -20,14 +20,13 @@ while True:
     player_input = input()
 
     while player_input not in ['1', '2', '3']:
-            print('Not a valid number, must choose "1", "2", or "3"')
-            player_input = input()
+        print('Not a valid number, must choose "1", "2", or "3"')
+        player_input = input()
 
     computer_pick = random.randrange(1, 4)
 
     print(f'You chose {choice(player_input)}')
     print(f'The computer chose {choice(computer_pick)}')
-    
 
     match (int(player_input), computer_pick):
         case (1, 2):
@@ -44,10 +43,7 @@ while True:
             print("You Win!")
         case _:
             print("You Tied!")
-    prompt("Play Again?")
+    prompt("Play Again? ('y' or 'n')")
     response = input()
     if response and response[0].lower() != 'y':
         break
-            
-        
-        
