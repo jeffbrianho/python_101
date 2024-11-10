bc81eb082bba6842c65446259d8ec8e5b039165b
Jeffrey Ho <jeffbrianho@gmail.com>
2024-11-10 16:54:28 -0600

    modified exercise 5 in easy 1 Small problems list

--- Small_Problems/easy_1/exercise5.py
+++ Small_Problems/easy_1/exercise5.py
@@ -2,17 +2,16 @@
 # The program must compute the tip, then print both the tip and the total amount of the bill. 
 # You can ignore input validation and assume that the user will enter valid numbers.
 
-def tip_percent(cost, tip):
-    return cost * tip
-
+def tip_percent():
+    tip = float(input('What is the tip percent? Enter in 0.2 for 20% '))
+                
 def bill_cost():
     print("Welcome to the bill calculator!")
-    bill = float(input("Enter in the cost of the bill"))
-    tip_rate = float(input("Enter in your tip amount"))
+    bill = float(input("Enter in the cost of the bill "))
+    
 
 # def display():
 #     print(f'The cost of the tip is{tip_percent(bill, tip_rate)}')
 #     print(f'The total cost is ')
-
 bill_cost()
-print(tip_percent(bill, tip_rate))
+tip_percent()
